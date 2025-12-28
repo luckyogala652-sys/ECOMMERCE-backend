2c62b28c0dea037248c10ce39c48f590aeca6928
Lucky Emmanuel <luckyogala652@gmail.com>
2025-12-28 23:54:49 +0100

    Changed image path

--- data/products.js
+++ data/products.js
@@ -3,35 +3,35 @@ const products = [
     id: 1,
     name: 'Milo',
     price: 5.99,
-    image: '/public/images/milo.jpg',
+    image: '/images/milo.jpg',
     category: 'Beverages'
   },
   {
     id: 2,
     name: 'Nivea',
     price: 7.23,
-    image: '/public/images/nivea.jpg',
+    image: '/images/nivea.jpg',
     category: 'Body-Cream'
   },
   {
     id: 3,
     name: 'Ice Cream',
     price: 2.81,
-    image: '/public/images/icecream.jpg',
+    image: '/images/icecream.jpg',
     category: 'Desserts'
   },
   {
     id: 4,
     name: 'Eva Soap',
     price: 1.99,
-    image: '/public/images/evasoap.jpg',
+    image: '/images/evasoap.jpg',
     category: 'Toiletries'
   },
   {
     id: 5,
     name: 'Cabin Biscuit',
     price: 3.50,
-    image: '/public/images/cabinbiscuit.jpg',
+    image: '/images/cabinbiscuit.jpg',
     category: 'Snacks'
   }
 ];
